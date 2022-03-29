4749f4d5f8e6b650ca231bca35b18914a0677065
Abdulrahman Altesha <aaltesha@msn.com>
2022-03-29 12:44:54 +0300

    29.03.2022

---
+++ www/js/routes.js
@@ -0,0 +1,13 @@
+var routes = [
+  {
+    "path": "/",
+    "url": "./index.html",
+    "name": "index"
+  },
+  {
+    "path": "/perm",
+    "page": "perm",
+    "url": "./perm.ejs",
+    "name": "perm"
+  }
+]
